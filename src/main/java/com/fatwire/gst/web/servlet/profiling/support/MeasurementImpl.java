f25267bb29cc6322a701c84c750f38d25641ad79
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-02 17:08:39 +0000

    Fixing major big with elapsed, epoch and nano seconds do not relate in a time elapsed sense

--- src/main/java/com/fatwire/gst/web/servlet/profiling/support/MeasurementImpl.java
+++ src/main/java/com/fatwire/gst/web/servlet/profiling/support/MeasurementImpl.java
@@ -1,46 +1,63 @@
-/**
- * 
- */
 package com.fatwire.gst.web.servlet.profiling.support;
 
 import com.fatwire.gst.web.servlet.profiling.Hierarchy;
 import com.fatwire.gst.web.servlet.profiling.Measurement;
 
+/**
+ * @author Dolf.Dijkstra
+ * 
+ */
 public final class MeasurementImpl implements Measurement {
     private final Hierarchy key;
 
-    final long startNanoTime = System.nanoTime();
+    final long startNanoTime;
 
-    final long startTime = System.currentTimeMillis();
+    final long startTime;
 
     long endTime;
 
     boolean running = true;
 
     MeasurementImpl(String key) {
-        this.key = new Hierarchy(key);
+        this(new Hierarchy(key));
     }
 
     public MeasurementImpl(Hierarchy name) {
         this.key = name;
+        startNanoTime = System.nanoTime();
+
+        startTime = System.currentTimeMillis();
+
     }
 
+    /* (non-Javadoc)
+     * @see com.fatwire.gst.web.servlet.profiling.Measurement#elapsed()
+     */
     public long elapsed() {
         if (running)
             return System.nanoTime() - startNanoTime;
-        return endTime - startTime;
+        return endTime - startNanoTime;
     }
 
+    /* (non-Javadoc)
+     * @see com.fatwire.gst.web.servlet.profiling.Measurement#stop()
+     */
     public void stop() {
         endTime = System.nanoTime();
         running = false;
 
     }
 
+    /* (non-Javadoc)
+     * @see com.fatwire.gst.web.servlet.profiling.Measurement#getName()
+     */
     public Hierarchy getName() {
         return key;
     }
 
+    /* (non-Javadoc)
+     * @see com.fatwire.gst.web.servlet.profiling.Measurement#getStartTime()
+     */
     public long getStartTime() {
         return startTime;
     }
