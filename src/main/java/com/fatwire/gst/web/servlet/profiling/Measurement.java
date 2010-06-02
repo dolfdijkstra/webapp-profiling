ab3d09f72f67543778018dd6c84f7f7ae6a8cae4
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-02 17:03:55 +0000

    Add javadoc

--- src/main/java/com/fatwire/gst/web/servlet/profiling/Measurement.java
+++ src/main/java/com/fatwire/gst/web/servlet/profiling/Measurement.java
@@ -2,15 +2,29 @@ package com.fatwire.gst.web.servlet.profiling;
 
 public interface Measurement {
 
+    /**
+     * 
+     * 
+     * @return the Hierarchy name of this measurement
+     */
     Hierarchy getName();
-    
+
+    /**
+     * 
+     * @return the time the measurement was started in epoch
+     */
     long getStartTime();
+
+    /**
+     * stop the measurement
+     */
 
     void stop();
 
-    /*
-     * elapsed time in nanoseconds since start or elepsed time between start() and stop()
+    /**
+     * elapsed time in nanoseconds since start or elepsed time between creation and stop()
      */
+
     long elapsed();
 
 }
