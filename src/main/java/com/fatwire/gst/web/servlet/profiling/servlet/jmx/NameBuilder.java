d503902ecc22317d5aace6b16875f10405c77884
Dolf Dijkstra <dolf.dijkstra@xs4all.nl>
2013-11-04 11:45:50 +0100

    Removing commented code

--- src/main/java/com/fatwire/gst/web/servlet/profiling/servlet/jmx/NameBuilder.java
+++ src/main/java/com/fatwire/gst/web/servlet/profiling/servlet/jmx/NameBuilder.java
@@ -21,30 +21,16 @@ import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 
 public class NameBuilder {
+    private static final String UTF_8 = "UTF-8";
     private final String[] parameters = new String[] { "pagename", "blobtable" };
+
     String sanitize(String s) {
         return s.replaceAll("[,=:\"*?]", "_");
     }
 
     String extractName(HttpServletRequest request) {
 
-        StringBuilder b = new StringBuilder(",path=").append(request
-                .getRequestURI());
-        //        if (request.getQueryString() != null) {
-        //            for (String part : request.getQueryString().split("&")) {
-        //                if (part.startsWith("pagename=")) {
-        //                    b.append(',');
-        //                    try {
-        //                        b.append(java.net.URLDecoder.decode(part, "UTF-8"));
-        //                    } catch (UnsupportedEncodingException e) {
-        //                        b.append(part);
-        //                    }
-        //                } else if (part.startsWith("blobtable=")) {
-        //                    b.append(',').append(part);
-        //                }
-        //            }
-        //
-        //        }
+        StringBuilder b = new StringBuilder(",path=").append(request.getRequestURI());
 
         for (String a : parameters) {
             if (request.getParameter(a) != null) {
@@ -52,8 +38,7 @@ public class NameBuilder {
                 b.append(a);
                 b.append('=');
                 try {
-                    b.append(java.net.URLDecoder.decode(
-                            request.getParameter(a), "UTF-8"));
+                    b.append(java.net.URLDecoder.decode(request.getParameter(a), UTF_8));
                 } catch (UnsupportedEncodingException e) {
                     b.append(request.getParameter(a));
                 }
